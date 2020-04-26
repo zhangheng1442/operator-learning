165dca6adafa9adeb9c1f37eb4fb031bdd716f02
zhangheng1442 <zhangheng1442@163.com>
2020-04-26 13:53:17 +0800

    init commit

---
+++ opdemo/pkg/controller/add_appservice.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+	"github.com/zhangheng1442/operator-learning/opdemo/pkg/controller/appservice"
+)
+
+func init() {
+	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+	AddToManagerFuncs = append(AddToManagerFuncs, appservice.Add)
+}
